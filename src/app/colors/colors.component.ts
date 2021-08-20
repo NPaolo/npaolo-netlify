818ce3e09d02ba5fb6a9e3df77207a331c3e9807
Paolo Nazzaro <paolonazzaro23@gmail.com>
2021-08-21 00:14:00 +0200

    colors + project-img

--- src/app/colors/colors.component.ts
+++ src/app/colors/colors.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { ThemeService } from '../theme.service';
+
 
 @Component({
   selector: 'app-colors',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./colors.component.css']
 })
 export class ColorsComponent implements OnInit {
+mode : boolean;
 
-  constructor() { }
+constructor(private _snackBar: MatSnackBar, private x : Router, private ts: ThemeService) {}
 
-  ngOnInit(): void {
+openSnackBar(message: string, action: string) {
+  this._snackBar.open(message, action, {
+    duration: 2000,
+  });
+}
+
+ngOnInit(): void {
+  if(this.ts.modeBool === true) {
+    this.mode = true;
+  } else { this.mode = false}
+  console.log(this.ts.modeBool)
+  console.log(this.mode)
+}
+
+switchMode() {
+  this.ts.switchMode();
+  this.mode = this.ts.modeBool;
+  console.log(this.mode)
+}
+
+  copyMessage(val: string){
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
+
+  goHome(){
+    this.x.navigate(['/'])
   }
 
 }
