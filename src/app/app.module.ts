37bc8c65be00045310373908050f27b92e2e1733
paolo <paolonazzaro23@gmail.com>
2022-03-07 15:53:35 +0100

    cv update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-
 // COMPONENTI
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
